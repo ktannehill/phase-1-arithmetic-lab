3f20debb4912393157b8b4d41fcfdc52a52ab215
Kat Tannehill <katetannehill@gmail.com>
2023-09-11 18:31:26 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 28;
+let num4 = 12;
+let mod = num3 % num4;
+
+let max = Math.max(1, 5, 20);
